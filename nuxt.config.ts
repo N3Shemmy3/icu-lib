ed14bb4da8c768f078ee9644d62588dee38befa6
Shemmy <shemmynyirenda3@gmail.com>
2025-02-21 20:15:16 +0200

    removed supabase because i will use a php backend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,26 +12,13 @@ export default defineNuxtConfig({
   },
   ssr: false,
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/icon",
-    "@nuxtjs/supabase",
-    "@nuxtjs/tailwindcss",
-    "@vueuse/nuxt",
-  ],
+  modules: ["@nuxt/icon", "@nuxtjs/tailwindcss", "@vueuse/nuxt"],
   runtimeConfig: {
     public: {
-      // Publicly accessible variables
+      /* Publicly accessible variables
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseKey: process.env.SUPABASE_KEY,
-    },
-  },
-  supabase: {
-    redirectOptions: {
-      login: "/auth/login",
-      callback: "/auth/confirm",
-      include: undefined,
-      exclude: ["*"],
-      cookieRedirect: false,
+      */
     },
   },
 });
